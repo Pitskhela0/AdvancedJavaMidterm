45e0aa3e0cea957085ecce91b023ef6916c4c242
Gakha Pitskhelauri <120589386+Pitskhela0@users.noreply.github.com>
2025-04-16 16:52:26 +0400

    updated y parameter getter

--- src/main/java/org/example/piece_attributes/Position.java
+++ src/main/java/org/example/piece_attributes/Position.java
@@ -2,18 +2,23 @@ package org.example.piece_attributes;
 
 public class Position {
     private int x;
-    private int y;
+    private char y;
 
-    public Position(int x, int y){
-        this.x = x;
+    public Position(char y, int x){
         this.y = y;
+        this.x = x;
     }
 
     public int getX() {
-        return x;
+        return this.x;
     }
 
     public int getY() {
-        return y;
+        return this.y -'a'+1;
+    }
+
+    @Override
+    public String toString() {
+        return ""+y+"  "+x;
     }
 }
